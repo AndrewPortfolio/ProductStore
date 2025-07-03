98969e1ee05e7c8cdbd8e0534c7bf372c36cdd14
Andrew Pham <andrewpham@Andrews-Mac-mini.local>
2025-07-02 20:25:37 -0700

    Frontend Progressing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { Box } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
+import Navbar from "./parts/Navbar";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Box minH={"100vh"}>
+      <Navbar /> 
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
+  )
+}
+
+export default App;
